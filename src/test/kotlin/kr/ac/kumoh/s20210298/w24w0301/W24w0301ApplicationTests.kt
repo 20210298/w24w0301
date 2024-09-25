c91fc0dcd24143d1eabcdf3caacd971c624e0ea3
20210298 <144835269+20210298@users.noreply.github.com>
2024-09-25 13:38:43 +0900

    🎉 시작

---
+++ src/test/kotlin/kr/ac/kumoh/s20210298/w24w0301/W24w0301ApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20210298.w24w0301
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24w0301ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
